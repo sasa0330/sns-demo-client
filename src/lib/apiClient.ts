5814079b2359b6912421238db73d113acb4405a2
sasamori.yu <sasa0330y@gmail.com>
2023-07-16 15:20:56 +0900

    BAEURL

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:5050/api",
+  //base:https://dashboard.render.com/
+  baseURL: process.env.NEXT_PUBLIC_API_BASEURL,
   headers: {
     "Content-type": "application/json",
   },
